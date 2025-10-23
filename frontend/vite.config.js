461749fbc0f08c7f407402e1fac860c0c3d7396e
Dayanch437 <dayanchkemalovich@gmail.com>
2025-10-23 10:26:35 +0500

    fix

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,22 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 1000, // Increase warning limit to 1MB
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules/react') || id.includes('node_modules/react-dom')) {
+            return 'vendor-react'
+          }
+          if (id.includes('node_modules/axios')) {
+            return 'vendor-axios'
+          }
+          if (id.includes('node_modules/antd')) {
+            return 'antd'
+          }
+        },
+      },
+    },
+  },
 })
